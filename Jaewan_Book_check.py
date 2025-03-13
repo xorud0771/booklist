339cfc1bfd185ef6b56a4680f55ea3bfed302b02
surim1212 <surim1212@gmail.com>
2025-03-13 16:45:27 +0900

    도서 수정 함수 수정

--- Jaewan_Book_check.py
+++ Jaewan_Book_check.py
@@ -1,5 +1,5 @@
 from operator import index
-import sys  # 프로그램 종료를 위해 sys 모듈 사용
+import Book_Checking_func as bcf  # 프로그램 종료를 위해 sys 모듈 사용
 
 # 사용자에게 보여줄 메뉴 화면
 display = '''
@@ -38,68 +38,8 @@ while True:
         print("도서가 추가되었습니다.")
 
     elif menu == '2':  # 도서 수정 기능
-        while True:
-            # 도서 리스트 출력 (수정할 때마다 최신 리스트를 보여줌)
-            print("\n현재 보유 중인 도서 리스트:")
-            for i in range(len(books)):
-                print(f"{i + 1}. {books[i]['제목']} (ISBN: {books[i]['ISBN']}, 저자: {books[i]['저자']}, 대여 여부: {books[i]['대여 여부']})")
-            
-            # 수정할 도서 번호 입력
-            book_num = input('수정할 도서 번호 입력 > ').strip()
-
-            if 1 <= int(book_num) <= len(books):
-                book_num = int(book_num) - 1  # 인덱스는 0부터 시작하므로
-                break
-            else:
-                print("해당 번호가 없습니다. 다시 입력해주세요.")
-
-        while True:
-            print("\n수정할 항목을 선택하세요:")
-            print("1. ISBN")
-            print("2. 제목")
-            print("3. 저자")
-            
-            number = int(input('수정할 부분 > ').strip())
-
-            if 1 <= number <= 4:
-                break
-            else:
-                print("잘못된 번호입니다. 1~4 사이의 번호를 입력해주세요.")
-
-        # 도서 수정 부분
-        if number == 1:
-            while True:
-                new_isbn = input(f'새 ISBN을 입력하세요 (기존 ISBN: {books[book_num]["ISBN"]}): ').strip()
-                
-                # ISBN 중복 체크
-                isbn_exists = False
-                for book in books:
-                    if book["ISBN"] == new_isbn:
-                        isbn_exists = True
-                        break
-                
-                if new_isbn == books[book_num]["ISBN"]:
-                    print("ISBN이 기존과 동일합니다. 다시 입력하세요.")
-                elif isbn_exists:
-                    print("이미 존재하는 ISBN입니다. 다른 ISBN을 입력해주세요.")
-                else:
-                    books[book_num]["ISBN"] = new_isbn
-                    print(f"ISBN이 수정되었습니다: {books[book_num]['ISBN']}")
-                    break
-
-        elif number == 2:
-            new_title = input(f'새 제목을 입력하세요 (기존 제목: {books[book_num]["제목"]}): ').strip()
-            books[book_num]["제목"] = new_title
-            print(f"제목이 수정되었습니다: {books[book_num]['제목']}")
-
-        elif number == 3:
-            new_author = input(f'새 저자를 입력하세요 (기존 저자: {books[book_num]["저자"]}): ').strip()
-            books[book_num]["저자"] = new_author
-            print(f"저자가 수정되었습니다: {books[book_num]['저자']}")
-
-
-        # 수정 후 최신 도서 정보 출력
-        print(f"수정된 도서 정보: {books[book_num]}")
+        print('책 정보 수정')
+        books = bcf.book_update(books)
 
     elif menu == '3':  # 도서 삭제 기능
         print('\n도서 삭제')
